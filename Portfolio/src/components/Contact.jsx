79205d8d44fc6a8e1d0ab4cf3b78c2d00ee092ef
Aman Kumar <amancollege04@gmail.com>
2025-01-21 13:02:21 +0530

    Implement contact form submission with feedback messages

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Contact = () => {
+
+    const [result, setResult] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending..");
+        
+        const formData = new FormData(event.target);
+        
+        formData.append("access_key", "cc329a4e-142d-45e5-90c3-b964aa88f312");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if(data.success){
+            setResult("Form Submitted Successfully..");
+            setTimeout(() => {
+                setResult("");
+            }, 2000)
+            event.target.reset();
+        }
+        else{
+            console.log("Error", data);
+            setResult(data.message);
+            setTimeout(() => {
+                setResult("");
+            }, 2000)
+        }
+    };
+
     return (
         <div className='w-full min-h-screen bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200 p-8 pt-24'>
             <h2 className='text-center font-bold text-4xl mb-8 mt-4'>Contact Me</h2>
             <div className='max-w-2xl mx-auto bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg'>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className='mb-4'>
                         <label className='block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2' htmlFor='name'>
                             Name
@@ -48,6 +83,7 @@ const Contact = () => {
                         </button>
                     </div>
                 </form>
+                <span>{result}</span>
             </div>
         </div>
     );
